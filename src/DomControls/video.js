d49db0b900d47f2d81c42823a1e45f6492ddb76e
NajafMohammed <najafmohammed17@gmail.com>
2023-03-29 07:34:51 +0530

    added ambient lighting to video

--- src/DomControls/video.js
+++ src/DomControls/video.js
@@ -1,16 +1,27 @@
-export const initVideo = (files, video) => {
+export const initVideo = (files, video, ambient) => {
   let source = document.createElement("source");
+  let ambientSource = document.createElement("source");
   const albumArt = document.getElementById("album-art-image");
   const fileExt = files[0].name.split(".").pop();
   if (fileExt === "mp3" || fileExt === "flac") {
     albumArt.style.display = "block";
     video.src = "";
+    ambient.src = "";
   } else {
     albumArt.style.display = "none";
+
     source.setAttribute("src", URL.createObjectURL(files[0]));
     source.setAttribute("type", "video/mp4");
+
+    ambientSource.setAttribute("src", URL.createObjectURL(files[0]));
+    ambientSource.setAttribute("type", "video/mp4");
+
     video.src = URL.createObjectURL(files[0]);
     video.play();
     video.muted = true;
+
+    ambient.src = URL.createObjectURL(files[0]);
+    ambient.play();
+    ambient.muted = true;
   }
 };
